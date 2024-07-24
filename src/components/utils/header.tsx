b1c8a385f31899977faa60c4e43e1be1b76f8943
Anshuman Singh <anshumaansingh98@gmail.com>
2024-07-24 23:26:00 +0530

    Header Modification
    Modified Header file for adding header and configured bootstrap and bootstrap icons

---
+++ src/components/utils/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './header.css';
+const Header = () => {
+    return(
+        <div className="border">
+            <i className="bi bi-phone"></i>
+        </div>
+    )
+}
+
+export default Header;
